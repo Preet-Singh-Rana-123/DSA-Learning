503e058bac86df09a624557b3aef77cbe6484a03
Preet Rana <preetrana1263@gmail.com>
2026-01-20 23:34:14 +0530

    solve remove Consecutive Same string from array question

--- Stack/stringManipulation.cpp
+++ Stack/stringManipulation.cpp
@@ -4,6 +4,14 @@
 #include <vector>
 using namespace std;
 
+// Tom is a string freak. He has got sequences of words arr[] to manipulate. If
+// in a sequence, two same words come together then Tom destroys each other.
+// Find the number of words left in the sequence after this pairwise
+// destruction.
+// -------------------------------------------------------------------------
+// To solve this we simply take a stack and then check if top of stack is equal
+// to arr[i] if yes then we pop that element from stack and if not then we push
+// that element in stack.
 int removeConsecutiveSame(vector<string> &arr) {
   stack<string> s;
   s.push(arr[0]);
