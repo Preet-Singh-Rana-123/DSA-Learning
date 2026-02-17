6a52a63a72e376d409a28c55c621b25f2fd333fc
Preet Rana <preetrana1263@gmail.com>
2026-02-17 19:28:58 +0530

    learn about dijkstra algorithm and implemented it using priority queue

--- Graph/dijkstraAlgorithm.cpp
+++ Graph/dijkstraAlgorithm.cpp
@@ -1,5 +1,7 @@
 #include <climits>
+#include <functional>
 #include <iostream>
+#include <queue>
 #include <utility>
 #include <vector>
 using namespace std;
@@ -61,10 +63,71 @@ vector<int> dijkstraAlgo1(int V, int src, vector<vector<int>> &edges) {
   return distance;
 }
 
+// --------- METHOD 2 -------------
+// To solve this problem we follow the bellow approach:-
+// 1. Select the node which in not explored yet and has minimum distance among
+// all unexplored node.
+// This time we make a priority_queue with min heap that
+// store minimum weight node as top element so that we can not have to perform
+// searching operation to find minimum weighted node. We can simply get
+// priority_queue's top.
+// 2. Now we relax the edges:-
+//    i. Look at all unexplored nodes.
+//    ii. if(distance[neighbour] > distance[node] + weight) then:
+//           distance[neighbour] = distance[node] + weight
+
+vector<int> dijkstraAlgo2(int V, int src, vector<vector<int>> &edges) {
+  vector<vector<pair<int, int>>> adj(V);
+  for (int i = 0; i < edges.size(); i++) {
+    int u = edges[i][0];
+    int v = edges[i][1];
+    int weight = edges[i][2];
+
+    adj[u].push_back({v, weight});
+    adj[v].push_back({u, weight});
+  }
+
+  vector<bool> explored(V, 0);
+  vector<int> distance(V, INT_MAX);
+  distance[src] = 0;
+
+  // creating priority_queue with min heap with pair of distance and node.
+  priority_queue<pair<int, int>, vector<pair<int, int>>,
+                 greater<pair<int, int>>>
+      p;
+  p.push({0, src}); // pushing source node and its distance as 0.
+
+  while (!p.empty()) {
+    // getting top of priority_queue
+    int node = p.top().second;
+    p.pop();              // poping top of priority_queue
+    if (explored[node]) { // checking if that node is explored or not.
+      continue;
+    }
+
+    explored[node] = 1; // marking that node explored.
+
+    // rest is same as above, here we relaxing edges
+    for (int i = 0; i < adj[node].size(); i++) {
+      int neighbour = adj[node][i].first;
+      int weight = adj[node][i].second;
+
+      if (!explored[neighbour] &&
+          distance[neighbour] > distance[node] + weight) {
+        distance[neighbour] = distance[node] + weight;
+        p.push({distance[neighbour],
+                neighbour}); // now just pushing neighbour into priority_queue
+      }
+    }
+  }
+
+  return distance;
+}
+
 int main() {
   vector<vector<int>> edges = {
       {0, 1, 4}, {0, 2, 8}, {1, 4, 6}, {2, 3, 2}, {3, 4, 10}};
-  vector<int> distance = dijkstraAlgo1(5, 0, edges);
+  vector<int> distance = dijkstraAlgo2(5, 0, edges);
   for (int i = 0; i < distance.size(); i++) {
     cout << distance[i] << " ";
   }
