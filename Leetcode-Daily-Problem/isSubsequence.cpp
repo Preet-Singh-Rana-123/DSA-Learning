fb76569ff528471956c175bce50ee1013eb452a7
Preet Rana <preetrana1263@gmail.com>
2026-04-19 22:45:24 +0530

    solve leetcode daily problem 392. Is Subsequence

---
+++ Leetcode-Daily-Problem/isSubsequence.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isSubsequence(string s, string t) {
+  int i = 0, j = 0;
+  while (i < s.size() && j < t.size()) {
+    if (s[i] == t[j]) {
+      i++;
+      j++;
+    } else {
+      j++;
+    }
+  }
+
+  return i == s.size() ? true : false;
+}
+
+int main() {
+  string s = "abc";
+  string t = "ahbgdc";
+  cout << "String 's' is subsequence of String 't': " << isSubsequence(s, t)
+       << "\n";
+  return 0;
+}
