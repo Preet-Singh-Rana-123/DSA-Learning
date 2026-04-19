fd441431483a58643b83a33e8065e91bc40272f8
Preet Rana <preetrana1263@gmail.com>
2026-04-19 22:39:23 +0530

    explain leetcode daily problem 1855. Maximum Distance Between a Pair of Values

--- Leetcode-Daily-Problem/maxDistPair.cpp
+++ Leetcode-Daily-Problem/maxDistPair.cpp
@@ -3,6 +3,27 @@
 #include <vector>
 using namespace std;
 
+// You are given two non-increasing 0-indexed integer arrays nums1 and nums2.
+
+// A pair of indices (i, j), where 0 <= i < nums1.length and 0 <= j <
+// nums2.length, is valid if both i <= j and nums1[i] <= nums2[j]. The distance
+// of the pair is j - i.
+
+// Return the maximum distance of any valid pair (i, j). If there are no valid
+// pairs, return 0.
+
+// An array arr is non-increasing if arr[i-1] >= arr[i] for every 1 <= i <
+// arr.length.
+//
+// -----------------------------------------------------------------------------
+// To solve this problem we use tow pointer approach.
+// As it is given that both num1 and num2 vector ar non-increasing order.
+// Since according to condition nums1[i]<=nums2[j] therefore we move i pointer
+// only when nums1[i]>nums2[j].
+// Also j will move when we have condition
+// nums1[i]<=nums2[j] because we have to max the distance b/w i & j so as i is
+// fixed we move j until we get nums1[i]>nums2[j].
+
 int maxDistance(vector<int> &nums1, vector<int> &nums2) {
   int maxDist = 0;
   int i = 0, j = 0;
