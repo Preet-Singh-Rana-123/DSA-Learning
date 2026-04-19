450cd4b9da7d9a87e74471150f64a777d6c96252
Preet Rana <preetrana1263@gmail.com>
2026-04-19 22:48:22 +0530

    explain leetcode daily problem 392. Is Subsequence

--- Leetcode-Daily-Problem/isSubsequence.cpp
+++ Leetcode-Daily-Problem/isSubsequence.cpp
@@ -2,6 +2,20 @@
 #include <string>
 using namespace std;
 
+// Given two strings s and t, return true if s is a subsequence of t, or false
+// otherwise.
+
+// A subsequence of a string is a new string that is formed from the original
+// string by deleting some (can be none) of the characters without disturbing
+// the relative positions of the remaining characters. (i.e., "ace" is a
+// subsequence of "abcde" while "aec" is not).
+//------------------------------------------------------------------------------
+// To solve this problem we use two pointer approach.
+// So when we found match for character we move both i and j pointer.
+// When we not, we just move j pointer.
+// In return if i become equal to size of subsequence then it is present, else
+// not.
+
 bool isSubsequence(string s, string t) {
   int i = 0, j = 0;
   while (i < s.size() && j < t.size()) {
