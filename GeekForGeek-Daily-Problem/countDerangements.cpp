1ddaa5f437057c068bbd8a1586e934053bb47d87
Preet Rana <preetrana1263@gmail.com>
2026-04-20 10:50:19 +0530

    solve geekForGeek count derangements problem

--- GeekForGeek-Daily-Problem/countDerangements.cpp
+++ GeekForGeek-Daily-Problem/countDerangements.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// Given a number n, find the total number of Derangements of elements from 1 to
+// n. A Derangement is a permutation of n elements, such that no element appears
+// in its original position, i.e., 1 should not be the first element, 2 should
+// not be second, etc. For example, [5, 3, 2, 1, 4] is a Derangement of first 5
+// elements.
+
+// Note: The answer will always fit into a 32-bit integer.
+// ----------------------------------------------------------------------
+// Recursive function for Derangement:- (n-1)*(D(n-1) + D(n-2))
+// Also for 1 there is no Derangement because 1 is allways at its position.
+// For 2 the derangement is always 1
+
 int derangeCount(int n) {
   if (n == 1) {
     return 0;
