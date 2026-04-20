a69c53f3494c20a2c7e5b2d2d9a65c7f81405b59
Preet Rana <preetrana1263@gmail.com>
2026-04-20 11:01:02 +0530

    explain leetcode daily problem 2078. Two Furthest Houses With Different Colors

--- Leetcode-Daily-Problem/differentColorHouse.cpp
+++ Leetcode-Daily-Problem/differentColorHouse.cpp
@@ -3,12 +3,25 @@
 #include <vector>
 using namespace std;
 
+// There are n houses evenly lined up on the street, and each house is
+// beautifully painted. You are given a 0-indexed integer array colors of length
+// n, where colors[i] represents the color of the ith house.
+
+// Return the maximum distance between two houses with different colors.
+
+// The distance between the ith and jth houses is abs(i - j), where abs(x) is
+// the absolute value of x.
+// ------------------------------------------------------------------------------
+// If we see here we can get that to get max distance for different colors
+// houses we have to compare all house with first and last lements.
+
 int maxDistance(vector<int> &colors) {
   int n = colors.size();
   int i = 0;
   int j = n - 1;
   int maxDist = 0;
 
+  // comparing all elements with first, starting from last.
   while (i < n && j >= 0) {
     if (colors[i] != colors[j]) {
       maxDist = max(maxDist, j - i);
@@ -18,6 +31,7 @@ int maxDistance(vector<int> &colors) {
   }
   i = 0;
   j = n - 1;
+  // comparing all elements with last, starting from first.
   while (i < n && j >= 0) {
     if (colors[i] != colors[j]) {
       maxDist = max(maxDist, j - i);
