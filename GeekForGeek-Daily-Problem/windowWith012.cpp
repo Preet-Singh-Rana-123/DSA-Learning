ff40310d6e8a9400603017b22337232dc10d6e60
Preet Rana <preetrana1263@gmail.com>
2026-04-27 22:46:07 +0530

    explain geekForGeek Smallest window containing 0, 1 and 2 question

--- GeekForGeek-Daily-Problem/windowWith012.cpp
+++ GeekForGeek-Daily-Problem/windowWith012.cpp
@@ -4,34 +4,49 @@
 #include <string>
 using namespace std;
 
-int smallestSubstring(string s){
-    int n = s.size();
+// Given a string s consisting only of the characters '0', '1' and '2',
+// determine the length of the smallest substring that contains all three
+// characters at least once.
 
-    int c0=0,c1=0,c2=0;
-    int left = 0;
-    int minLen = INT_MAX;
+// If no such substring exists, return -1.
+// --------------------------------------------------------------------------
 
-    for(int i=0;i<n;i++){
-        if(s[i]=='0') c0++;
-        else if(s[i]=='1') c1++;
-        else c2++;
+int smallestSubstring(string s) {
+  int n = s.size();
 
-        while(c0>0 && c1>0 && c2>0){
-            minLen = min(minLen,i-left+1);
+  int c0 = 0, c1 = 0, c2 = 0;
+  int left = 0;
+  int minLen = INT_MAX;
 
-            if(s[left]=='0') c0--;
-            else if(s[left]=='1') c1--;
-            else c2--;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == '0')
+      c0++;
+    else if (s[i] == '1')
+      c1++;
+    else
+      c2++;
 
-            left++;
-        }
+    while (c0 > 0 && c1 > 0 && c2 > 0) {
+      minLen = min(minLen, i - left + 1);
+
+      if (s[left] == '0')
+        c0--;
+      else if (s[left] == '1')
+        c1--;
+      else
+        c2--;
+
+      left++;
     }
+  }
 
-    return minLen==INT_MAX ? -1 : minLen;
+  return minLen == INT_MAX ? -1 : minLen;
 }
 
-int main(){
-    string s = "121100112011112";
-    cout<<"The length of the smallest substring that contains all three characters at least once: "<<smallestSubstring(s)<<"\n";
-    return 0;
+int main() {
+  string s = "121100112011112";
+  cout << "The length of the smallest substring that contains all three "
+          "characters at least once: "
+       << smallestSubstring(s) << "\n";
+  return 0;
 }
